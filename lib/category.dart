17352588141d3e90b34288839e70371b36d785b9
roshniRam <roshni.ram001@gmail.com>
2018-12-19 02:09:24 +0530

    Add categories list

--- lib/category.dart
+++ lib/category.dart
@@ -1,43 +1,39 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:meta/meta.dart';
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 
-const _categoryHeight = 100.0;
-final _borderRadius = BorderRadius.circular(_categoryHeight/2);
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 
 class Category extends StatelessWidget {
-
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
 
-  /// Creates a [Category].
-  ///
-  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
-  /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category({Key key,
-  @required this.name,
-  @required this.color,
-  @required this.iconLocation} ): assert(name !=null), assert(color != null), assert(iconLocation != null),super(key:key);
 
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  /// This information includes the icon, name, and color for the [Category].
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
+
   @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
       child: Container(
-        height: _categoryHeight,
+        height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
           highlightColor: color,
