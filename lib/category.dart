f411f5a2a4386d6194b0d6b13660504987421bd4
Roshni  Ram <30633549+roshniRam@users.noreply.github.com>
2018-12-19 02:50:13 +0530

    Add unit and layout for conversion (#2)

--- lib/category.dart
+++ lib/category.dart
@@ -2,20 +2,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:meta/meta.dart';
+import 'converter_route.dart';
+import 'unit.dart';
+
+
+
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
-
-final _rowHeight = 100.0;
-final _borderRadius = BorderRadius.circular(_rowHeight / 2);
-
-
 class Category extends StatelessWidget {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
+  final List<Unit> units;
 
 
   const Category({
@@ -23,12 +26,45 @@ class Category extends StatelessWidget {
     @required this.name,
     @required this.color,
     @required this.iconLocation,
+    @required this.units,
   })  : assert(name != null),
         assert(color != null),
         assert(iconLocation != null),
+        assert(units != null),
         super(key: key);
 
+  /// Navigates to the [ConverterRoute].
+  void _navigateToConverter(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            name: name,
+            units: units,
+          ),
+        );
+      },
+    ));
+  }
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
   @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
@@ -40,9 +76,7 @@ class Category extends StatelessWidget {
           splashColor: color,
           // We can use either the () => function() or the () { function(); }
           // syntax.
-          onTap: () {
-            print('I was tapped!');
-          },
+          onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
---
+++ lib/converter_route.dart
@@ -0,0 +1,50 @@
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+
+import 'unit.dart';
+
+class ConverterRoute extends StatelessWidget {
+  /// This [Category]'s name.
+  final String name;
+
+  /// Color for this [Category].
+  final Color color;
+
+  /// Units for this [Category].
+  final List<Unit> units;
+
+  /// This [ConverterRoute] requires the name, color, and units to not be null.
+  const ConverterRoute({
+    @required this.units,
+    @required this.name,
+    @required this.color,
+  })  : assert(units != null),assert(name != null),assert(color != null);
+
+  @override
+  Widget build(BuildContext context) {
+    // Here is just a placeholder for a list of mock units
+    final unitWidgets = units.map((Unit unit) {
+      return Container(
+        color: color,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
